e19a7dcf0dd10bb198e3fc303c2b90df5f87a621
Pita <pitakibunja@gmail.com>
2022-06-08 10:18:29 +0300

    changed app text

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          learn whatever you would like
         </a>
       </header>
     </div>
